35c1dc88594352c1b07ac1aa9cee7920012dd7df
rajasekhar <raj479.mits@gmail.com>
2025-02-05 03:32:09 +0530

    session-28

---
+++ vpc.tf
@@ -0,0 +1,8 @@
+module "vpc" {
+    source = "../10-terraform-aws-vpc"
+    project_name = var.project_name
+    environment = var.environment
+    vpc_cidr = var.vpc_cidr
+    common_tags = var.common_tags
+    vpc_tags = var.vpc_tags
+}
